c77de8c9003d1e8eb660ad120a5ea7b958904a8d
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-04-15 09:07:12 +0300

    scriere fisier Excel... kind of

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,11 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,5 +39,36 @@ public class Main {
             }
             System.out.println("");
         }
+
+        XSSFWorkbook worklook = new XSSFWorkbook();
+        XSSFSheet worksheet = worklook.createSheet("TestExcel");
+
+        Map<String, Object[]> data = new TreeMap<String, Object[]>();
+        data.put("5", new Object[] {"69", "Adolf", "Hitler"});
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+
+            Row row = sheet.createRow(rownum++);
+            Object [] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr)
+            {
+                Cell cell = row.createCell(cellnum++);
+                if(obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if(obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+            }
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(new File("howtodoinjava_demo.xlsx"));
+            worklook.write(out);
+            out.close();
+            System.out.println("howtodoinjava_demo.xlsx written successfully on disk.");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } //dupa sa luam varsta tuturor si sa o incrementam cu 10
     }
 }
